bec2ab204d7c9403205140f57dac90020e559053
Raghavan <raghavan.work@outlook.com>
2025-04-15 14:53:31 +0530

    init

--- confirmed/py note detector/module.py
+++
@@ -1,7 +0,0 @@
-# module.py
-def greet(name):
-    print(f"Hello, {name}!")
-
-
-if __name__ == "__main__":
-    print("This is the module.py file.")
--- sur_alap_detect/android/app/src/main/kotlin/com/example/sur_alap_detect/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sur_alap_detect
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
